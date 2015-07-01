c997dd6610db75e232d473d619380350ba91afaf
Veronica Ray <vray@linkedin.com>
2015-06-30 18:45:20 -0700

    Update cocoapods to latest, deintegrate cocoapods and then pod install. Fixed the Mach-O Linker Error

--- Pods/Target Support Files/Pods-ReporterProject-PNChart/Pods-ReporterProject-PNChart-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReporterProject_PNChart : NSObject
-@end
-@implementation PodsDummy_Pods_ReporterProject_PNChart
-@end
--- Pods/Target Support Files/Pods-ReporterProject-PureLayout/Pods-ReporterProject-PureLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReporterProject_PureLayout : NSObject
-@end
-@implementation PodsDummy_Pods_ReporterProject_PureLayout
-@end
